ce412c2bb4864378f78c8b9c18eaab97660c3c8a
daniel kacou <danleynewline@gmail.com>
2020-09-22 09:05:23 +0000

    fini animation reste contact

---
+++ js/reveal.js
@@ -0,0 +1,126 @@
+const sr = ScrollReveal({
+    reset: true,
+});
+
+sr.reveal('.header-home',{
+    duration: 2000,
+    scale: 0.8,
+    reset: false
+});
+
+sr.reveal('.carousel',{
+    duration: 2000,
+    scale: 0.8
+});
+
+sr.reveal('.head h1',{
+    duration: 4000,
+    scale: 0.5
+});
+sr.reveal('.head .first h2',{
+    duration: 5000,
+    scale: 0.4,
+    delay: 500
+});
+
+sr.reveal('.cursurs-home h1',{
+    origin : 'left',
+    delay  : 200,
+  distance : '120px',
+  easing   : 'ease-in-out',
+});
+
+sr.reveal('.diplome.one',{
+    duration: 2000,
+    scale: 0.4,
+    delay: 500,
+    easing: 'ease-in-out',
+    reset: false
+});
+sr.reveal('.diplome.two',{
+    duration: 2000,
+    scale: 0.4,
+    delay: 1700,
+    easing: 'ease-in-out',
+    reset: false
+});
+sr.reveal('.diplome.three',{
+    duration: 2000,
+    scale: 0.4,
+    delay: 3000,
+    easing: 'ease-in-out',
+    reset: false
+});
+sr.reveal('.diplome.four',{
+    duration: 2000,
+    scale: 0.4,
+    delay: 4200,
+    easing: 'ease-in-out',
+    reset: false
+});
+sr.reveal('.diplome.five',{
+    duration: 2000,
+    scale: 0.4,
+    delay: 5500,
+    easing: 'ease-in-out',
+    reset: false
+});
+sr.reveal('.diplome.six',{
+    duration: 2000,
+    scale: 0.4,
+    delay: 6700,
+    easing: 'ease-in-out',
+    reset: false
+});
+
+sr.reveal('.accueil .info',{
+    duration: 2000,
+    scale: 0.8,
+    delay: 1000,
+    origin: 'right',
+    distance: '10%',
+    easing: 'ease-in-out',
+
+});
+
+
+/**ECOLE Apropos */
+sr.reveal('.header-home h2',{
+    delay: 1000,
+    duration: 2000,
+    scale: 0.4,
+    origin: 'top',
+    distance: '100px',
+    reset: false
+});
+
+sr.reveal('.fondateur h1, .fondateur h2, .fondateur img',{
+  distance : '120px',
+  easing   : 'ease-in-out',
+});
+
+sr.reveal('.fondateur h1',{
+    origin : 'bottom',
+    delay  : 2000,
+});
+sr.reveal('.fondateur img',{
+    origin : 'bottom',
+    delay  : 2000,
+});
+
+sr.reveal('.text-fondateur, .present-escam',{
+    duration: 3000,
+    delay  : 1000,
+    distance: '10%',
+    reset:false,
+    origin : 'bottom',
+});
+
+
+sr.reveal('.certif h1',{
+    duration: 1000,
+    delay  : 3000,
+    distance: '20%',
+    origin : 'right',
+    reset:false
+});
